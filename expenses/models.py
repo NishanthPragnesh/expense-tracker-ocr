9ae25fd9edd86e28c34eff6290fb04b58996cd5f
nishanthpragnesh <nishanthpragnesh@gmail.com>
2025-07-03 10:54:50 +0530

    Phase 5: Modifed models.py so that it makes user specific

--- expenses/models.py
+++ expenses/models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
