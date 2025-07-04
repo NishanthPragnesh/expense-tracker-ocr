79aab647ccaa103d3a9124ed401e3b57d18fb29c
nishanthpragnesh <nishanthpragnesh@gmail.com>
2025-07-04 17:25:01 +0530

    Static files enabled

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -119,3 +119,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media files (user uploads)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
